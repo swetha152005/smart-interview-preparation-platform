9994819adcb92cf46e3fe51aef486b26abd7e785
swetha152005 <swethamarirajan@gmail.com>
2026-09-17 12:17:11 +0530

    Update database connection for Aiven

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,16 @@
 const mysql = require("mysql2");
+const fs = require("fs");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = db;
